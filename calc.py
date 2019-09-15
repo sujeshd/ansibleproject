cb386cd11eb6e8424a33b880aa9719ec7167002f
Ethans Institute <sujeshdamodaran1980@gmail.com>
2019-09-15 07:20:21 +0000

    IM

--- calc.py
+++ calc.py
@@ -15,7 +15,6 @@ def add(x,y):
 
                     def multiply(x,y):
                             return x*y
-
                         def divide (x,y):
                             pass
                           
